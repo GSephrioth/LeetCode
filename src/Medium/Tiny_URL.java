964df714b2823068fce1eaaa35a7d4e7cae9d70f
Elliott Chen <596620726@qq.com>
2017-07-05 16:35:04 +0800

    Working on Tiny_URL
    Signed-off-by: Elliott Chen <596620726@qq.com>

--- src/Medium/Tiny_URL.java
+++ src/Medium/Tiny_URL.java
@@ -1,7 +1,11 @@
 package Medium;
 
 /**
- *
+ * TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl.
+ * And it returns a short URL such as http://tinyurl.com/4e9iAk.
+ * Design the encode and decode methods for the TinyURL service.
+ * There is no restriction on how your encode/decode algorithm should work.
+ * You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
  * Created by xuzhuchen on 6/28/17.
  */
 public class Tiny_URL {
@@ -15,7 +19,6 @@ public class Tiny_URL {
     public String decode(String shortUrl) {
         return null;
     }
-
 
 // Your Codec object will be instantiated and called as such:
 // Codec codec = new Codec();
